d61f6a64acd1332b3787aebd79b0af499ccaa1d7
Ajay Rawat <a.rawat@saedubai.com>
2017-11-19 16:33:31 +0400

    Added headbobbing of the player and stamina sprinting depletion.

--- NecroPhobia/Assets/Scripts/CharacterController.cs
+++ NecroPhobia/Assets/Scripts/CharacterController.cs
@@ -9,27 +9,21 @@ public class CharacterController : MonoBehaviour
     public float walkingSpeed;
     public float runningSpeed;
 
+
     private int score = 0;
     public Text textScore;
 
+    PlayerStats ps;
+
     void Start()
     {
-
+        ps = GetComponent<PlayerStats>();
     }
 
 
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * walkingSpeed;
-        float straffe = Input.GetAxis("Horizontal") * walkingSpeed;
-
-        translation *= Time.deltaTime;
-        straffe *= Time.deltaTime;
-
-        transform.Translate(straffe, 0f, translation);
-
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && ps.currStamina > 0)
         {
             float translation2 = Input.GetAxis("Vertical") * runningSpeed;
             float straffe2 = Input.GetAxis("Horizontal") * runningSpeed;
@@ -38,6 +32,17 @@ public class CharacterController : MonoBehaviour
             straffe2 *= Time.deltaTime;
 
             transform.Translate(straffe2, 0f, translation2);
+        }
+
+        else
+        {
+            float translation = Input.GetAxis("Vertical") * walkingSpeed;
+            float straffe = Input.GetAxis("Horizontal") * walkingSpeed;
+
+            translation *= Time.deltaTime;
+            straffe *= Time.deltaTime;
+
+            transform.Translate(straffe, 0f, translation);
         }
 
     }
