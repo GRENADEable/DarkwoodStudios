6271a35c898bbc634904411259af476d0b5ec3cd
Joshua Asuncion <j.asuncion@saedubai.com>
2017-11-23 01:20:55 +0400

    Hey guys I just tried adding textures to some few props.

---
+++ NecroPhobia/Assets/Textures/Scripts/GameManagerLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLoader : MonoBehaviour
+{
+    public GameObject gameManage;
+
+    void Awake()
+    {
+        if (GameManager.gm == null)
+            Instantiate(gameManage);
+
+    }
+}
