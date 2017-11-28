c3501ebead18a99d9b90165f7bdd2b453ec0b253
Khatim <kahtim.dar@gmail.com>
2017-11-28 16:54:59 +0400

    Added the Behviour Tree Structure. Completed the Attack State. Still working on the Patrol and Chase State.

---
+++ NecroPhobia/Assets/Scripts/BT/EnemyBehaviourTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviourTree : MonoBehaviour
+{
+    [HideInInspector] public Node root;
+    public float enemySpeed;
+
+
+    public Transform player;
+    public float chaseDistance;
+    public float attackDistance;
+    public float distanceToPlayer;
+    public float angle;
+    [HideInInspector] public Vector3 tarDir;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Selector selectNode = new Selector();
+        Sequence sequenceNode = new Sequence();
+
+        root = selectNode;
+        selectNode.children.Add(sequenceNode);
+        selectNode.children.Add(new Patrol());
+
+        sequenceNode.children.Add(new Chase());
+        sequenceNode.children.Add(new Attack());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        root.Execute(this);
+
+    }
+}
---
+++ NecroPhobia/Assets/Scripts/BT/Patrol.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : Node
+{
+
+    public override void Execute(EnemyBehaviourTree ownerBT)
+    {
+
+    }
+}
